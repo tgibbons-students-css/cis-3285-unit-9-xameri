1d7c51695c64e3a0567322d5e9c2b439a927e12c
Farah Farah <afarah1@css.edu>
2017-11-06 13:19:06 -0600

    Request  407 check the update of creating a new class of urltradeprovider

--- SingleResponsibilityPrinciple/Program.cs
+++ SingleResponsibilityPrinciple/Program.cs
@@ -9,16 +9,17 @@ namespace SingleResponsibilityPrinciple
     {
         static void Main(string[] args)
         {
-            var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
+            //var tradeStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("SingleResponsibilityPrinciple.trades.txt");
 
+            string url = "http://faculty.css.edu/tgibbons/trades4.txt";
             var logger = new ConsoleLogger();
             var tradeValidator = new SimpleTradeValidator(logger);
-            var tradeDataProvider = new StreamTradeDataProvider(tradeStream);
+            var urlTradeDataProvider = new URLTradeDataProvider(url);
             var tradeMapper = new SimpleTradeMapper();
             var tradeParser = new SimpleTradeParser(tradeValidator, tradeMapper);
             var tradeStorage = new AdoNetTradeStorage(logger);
 
-            var tradeProcessor = new TradeProcessor(tradeDataProvider, tradeParser, tradeStorage);
+            var tradeProcessor = new TradeProcessor(urlTradeDataProvider, tradeParser, tradeStorage);
             tradeProcessor.ProcessTrades();
 
             Console.ReadKey();
---
+++ SingleResponsibilityPrinciple/URLTradeDataProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using SingleResponsibilityPrinciple.Contracts;
+
+namespace SingleResponsibilityPrinciple
+{
+   public class URLTradeDataProvider : ITradeDataProvider
+    {
+        public URLTradeDataProvider(Stream stream)
+        {
+            this.stream = stream;
+        }
+
+        public URLTradeDataProvider(string url)
+        {
+            this.url = url;
+        }
+
+
+
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = new List<string>();
+            var client = new WebClient();
+            using (var stream = client.OpenRead(url))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    tradeData.Add(line);
+                }
+            }
+            return tradeData;
+
+        }
+
+        private readonly Stream stream;
+        private string url;
+    }
+}
+
