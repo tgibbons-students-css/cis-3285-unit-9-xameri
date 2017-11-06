07ff1ec0495e9134fccd1631475c4510d26d99c9
Farah Farah <afarah1@css.edu>
2017-11-06 10:04:53 -0600

    Request 403 check lower bound

--- SingleResponsibilityPrinciple/SimpleTradeValidator.cs
+++ SingleResponsibilityPrinciple/SimpleTradeValidator.cs
@@ -33,6 +33,12 @@ namespace SingleResponsibilityPrinciple
                 return false;
             }
 
+            if (tradeAmount < 1000)
+            {
+                logger.LogWarning("Trade below 1000: '{0}'", tradeData[1]);
+                return false;
+            }
+
             decimal tradePrice;
             if (!decimal.TryParse(tradeData[2], out tradePrice))
             {
